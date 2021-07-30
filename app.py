1167a29f1a161ca313b6a390ec91973b12642d52
Abhishake Kumar Bojja <abojja@uvic.ca>
2021-07-30 16:58:03 -0400

    added try catch to handle missing data

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle, json
 
 
 app = Flask(__name__)
-with open('fraud_detection_saved_model.pkl','rb') as f:
+with open('model_dir/fraud_detection_saved_model.pkl','rb') as f:
     model = pickle.load(f)
 
 feature_keys = ["Time", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9",
@@ -20,46 +20,56 @@ def home():
 
 @app.route('/predict',methods = ['POST'])
 def predict():
+    try:
+        data = json.loads(list(request.form.values())[0])
+        features = []
+        for key in feature_keys:
+            if key in data:
+                features.append(float(data[key]))
+            else:
+                features.append(0)
 
-    data = json.loads(list(request.form.values())[0])
-    features = []
-    for key in feature_keys:
-        if key in data:
-            features.append(float(data[key]))
+        features_np = np.array(features).reshape(1, -1)
+        prediction = model.predict_proba(features_np)[0]
+        class_label = "Normal" if prediction[1] < 0.90 else  "Fraud" 
+        
+        if class_label == "Normal":
+            message = "[*] The transaction is Normal."
         else:
-            features.append(0)
-
-
-    # features = [float(data[key]) for key in feature_keys]
-    features_np = np.array(features).reshape(1, -1)
-    prediction = model.predict(features_np)
-    print(prediction[0])
-    class_label = "Normal" if prediction[0] == 0 else "Fraud"
-    if prediction[0] == 0:
-        message = "[*] The transaction is Normal."
-    else:
-        message = "[!] The transaction seems Fraud."
-    request.get_json()
+            message = "[!] The transaction seems Fraud."
+    except:
+            message = "The model failed"
     return render_template('home.html', prediction_text=f"{message}")
 
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-#     '''
-#     For direct API calls trought request
-#     '''
-#     data = request.get_json(force=True)
-#     prediction = model.predict([np.array(list(data.values()))])
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    try:
+        data = request.get_json(force=True)
+        features = []
+        for key in feature_keys:
+            if key in data:
+                try:
+                    features.append(float(data[key]))
+                except:
+                    features.append(0)
+            else:
+                features.append(0)
 
-#     output = prediction[0]
-#     return jsonify(output)
+        features_np = np.array(features).reshape(1, -1)
+        prediction = model.predict_proba(features_np)[0]
+        class_label = "Normal" if prediction[1] < 0.90 else  "Fraud" 
 
-# def load_features(data):
-#     output = []
-#     for key in feature_keys:
-#         output.append(float(data.get(key, 0))
-#     return output
-        
-
+        if class_label == "Normal":
+            message = "[*] The transaction is Normal."
+        else:
+            message = "[!] The transaction seems Fraud."
+    
+    except:
+        message = "The model failed"
+    return json.dumps({"message": message})
 
 
 if __name__ == '__main__':
